ea1d64d3d41cd2a847dabfe06a0f1c150163bcd2
npiskarev <npiskarev@PISKAREV>
2025-08-15 13:12:29 +0300

    Попытка привязать данные

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -46,6 +46,7 @@ namespace UncomClc.ViewModels
             {
                 _selectedPipeLine = value;
                 OnPropertyChanged(nameof(SelectedPipeLine));
+                LoadSelectedPipelineData();
             }
         }
 
@@ -80,7 +81,7 @@ namespace UncomClc.ViewModels
             {
                 Id = newId,
                 Name = $"NewLine_{newId}",
-                Parameters = new Parameters()
+                Parameters = UpdateCurrentParameters()
             };
 
             PipeLines.Add(newStructure);
@@ -118,7 +119,7 @@ namespace UncomClc.ViewModels
                 {
                     Id = 1,
                     Name = "NewLine",
-                    Parameters = new Parameters()
+                    Parameters = UpdateCurrentParameters()
                 };
                 PipeLines.Add(newStructure);
                 SelectedPipeLine = newStructure;
@@ -176,7 +177,7 @@ namespace UncomClc.ViewModels
                     Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
                     DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
                 };
-                UpdateCurrentParameters();
+                SaveCurrentParameters();
                 var json = JsonSerializer.Serialize(PipeLines, options);
                 File.WriteAllText(file, json);
                 MessageBox.Show("Файл успешно сохранен", "Успех", MessageBoxButton.OK, MessageBoxImage.Information);
@@ -187,10 +188,10 @@ namespace UncomClc.ViewModels
             }
         }
 
-        private void UpdateCurrentParameters()
+        private Parameters UpdateCurrentParameters()
         {
-            if (SelectedPipeLine == null) return;
-            var param = SelectedPipeLine.Parameters;
+            
+            var param = new Parameters();
             var pipe = Data.UploadedData.Instance.Pipes.Where(x => x.Name == ProcessVM.Pipe).FirstOrDefault();
             var isulation = Data.UploadedData.Instance.Insulations.Where(x => x.Name == ProcessVM.ThermalIsolation).FirstOrDefault();
             var isulation2 = Data.UploadedData.Instance.Insulations.Where(x => x.Name == ProcessVM.ThermalIsolation2).FirstOrDefault();
@@ -231,7 +232,100 @@ namespace UncomClc.ViewModels
             param.CableType = PowerSupplyParametersVM.CableType;
             param.NumberCores = PowerSupplyParametersVM.NumberCores;
             param.LenghtSection = PowerSupplyParametersVM.LenghtSection;
-
+            return param;
         }
+        private void SaveCurrentParameters()
+        {
+            if (SelectedPipeLine == null) return;
+            var param = SelectedPipeLine.Parameters;
+            var pipe = Data.UploadedData.Instance.Pipes.Where(x => x.Name == ProcessVM.Pipe).FirstOrDefault();
+            var isulation = Data.UploadedData.Instance.Insulations.Where(x => x.Name == ProcessVM.ThermalIsolation).FirstOrDefault();
+            var isulation2 = Data.UploadedData.Instance.Insulations.Where(x => x.Name == ProcessVM.ThermalIsolation2).FirstOrDefault();
+
+            param.Pipe = pipe;
+            param.ThermalIsolation = isulation;
+            param.ThermalIsolation2 = isulation2;
+            param.IsolationThickness = ProcessVM.IsolationThickness;
+            param.IsolationThickness2 = ProcessVM.IsolationThickness2;
+            param.Diam = ProcessVM.Diam;
+            param.Thickness = ProcessVM.Thickness;
+            param.Lenght = ProcessVM.Lenght;
+            param.PipeKoef = ProcessVM.PipeKoef;
+            param.ValveCount = ProcessVM.ValveCount;
+            param.ValveLenght = ProcessVM.ValveLenght;
+            param.SupportLenght = ProcessVM.SupportLenght;
+            param.SupportCount = ProcessVM.SupportCount;
+            param.FlangCount = ProcessVM.FlangCount;
+            param.FlangLength = ProcessVM.FlangLength;
+            param.MinEnvironmentTemp = EnvironmentVM.MinEnvironmentTemp;
+            param.MaxEnvironmentTemp = EnvironmentVM.MaxEnvironmentTemp;
+            param.PipelinePlacement = EnvironmentVM.PipelinePlacement;
+            param.SupportedTemp = PowerSupplyParametersVM.SupportedTemp;
+            param.MaxAddProductTemp = ProcessVM.MaxAddProductTemp;
+            param.MaxTechProductTemp = PowerSupplyParametersVM.MaxTechProductTemp;
+            param.SteamingStatus = ProcessVM.SteamingStatus;
+            param.StreamingTemperature = ProcessVM.StreamingTemperature;
+            param.TemperatureClass = ProcessVM.TemperatureClass;
+            param.TemperatureClassValue = ProcessVM.TemperatureClassValue;
+            param.WorkEnvironment = PowerSupplyParametersVM.WorkEnvironment;
+            param.PhaseVoltage = PowerSupplyParametersVM.PhaseVoltage;
+            param.LineVoltage = PowerSupplyParametersVM.LineVoltage;
+            param.Current = PowerSupplyParametersVM.Current;
+            param.Nutrition = PowerSupplyParametersVM.Nutrition;
+            param.WorkLoad = PowerSupplyParametersVM.WorkLoad;
+            param.ConnectionScheme = PowerSupplyParametersVM.ConnectionScheme;
+            param.MinTempOn = PowerSupplyParametersVM.MinTempOn;
+            param.CableType = PowerSupplyParametersVM.CableType;
+            param.NumberCores = PowerSupplyParametersVM.NumberCores;
+            param.LenghtSection = PowerSupplyParametersVM.LenghtSection;
+        }
+        private void LoadSelectedPipelineData()
+        {
+            if (SelectedPipeLine == null || SelectedPipeLine.Parameters == null) return;
+
+            // Блокируем обновление UI во время загрузки
+            var temp = SelectedPipeLine.Parameters;
+
+            // ProcessView
+            ProcessVM.Pipe = temp.Pipe?.Name;
+            ProcessVM.Diam = temp.Diam;
+            ProcessVM.Thickness = temp.Thickness;
+            ProcessVM.PipeKoef = temp.PipeKoef;
+            ProcessVM.Lenght = temp.Lenght;
+            ProcessVM.ThermalIsolation = temp.ThermalIsolation?.Name;
+            ProcessVM.ThermalIsolation2 = temp.ThermalIsolation2?.Name;
+            ProcessVM.IsolationThickness = temp.IsolationThickness;
+            ProcessVM.IsolationThickness2 = temp.IsolationThickness2;
+            ProcessVM.MaxAddProductTemp = temp.MaxAddProductTemp;
+            ProcessVM.SteamingStatus = temp.SteamingStatus;
+            ProcessVM.StreamingTemperature = temp.StreamingTemperature;
+            ProcessVM.TemperatureClassValue = temp.TemperatureClassValue;
+            ProcessVM.SupportCount = temp.SupportCount;
+            ProcessVM.SupportLenght = temp.SupportLenght;
+            ProcessVM.ValveCount = temp.ValveCount;
+            ProcessVM.ValveLenght = temp.ValveLenght;
+            ProcessVM.FlangCount = temp.FlangCount;
+            ProcessVM.FlangLength = temp.FlangLength;
+
+            // EnvironmentView
+            EnvironmentVM.MaxEnvironmentTemp = temp.MaxEnvironmentTemp;
+            EnvironmentVM.MinEnvironmentTemp = temp.MinEnvironmentTemp;
+            EnvironmentVM.PipelinePlacement = temp.PipelinePlacement;
+
+            // PowerSupplyParametersView
+            PowerSupplyParametersVM.SupportedTemp = temp.SupportedTemp;
+            PowerSupplyParametersVM.MaxTechProductTemp = temp.MaxTechProductTemp;
+            PowerSupplyParametersVM.WorkEnvironment = temp.WorkEnvironment;
+            PowerSupplyParametersVM.LineVoltage = temp.LineVoltage;
+            PowerSupplyParametersVM.Current = temp.Current;
+            PowerSupplyParametersVM.NumberCores = temp.NumberCores;
+            PowerSupplyParametersVM.LenghtSection = temp.LenghtSection;
+            PowerSupplyParametersVM.CableType = temp.CableType;
+            PowerSupplyParametersVM.MinTempOn = temp.MinTempOn;
+            PowerSupplyParametersVM.ConnectionScheme = temp.ConnectionScheme;
+            PowerSupplyParametersVM.Nutrition = temp.Nutrition;
+        }
+
     }
 }
+
